c01b37bdf9bb1cc9cec439f23052223ce5bfa20e
abhi18av <abhi18av@gmail.com>
2018-06-23 14:03:51 +0530

    added the scratch and resources in the gitignore

---
+++ project.clj
@@ -0,0 +1,6 @@
+(defproject book-keeper "0.1.0-SNAPSHOT"
+  :description "A clojure repl for managing my personal e-library"
+  :url "https://github.com/abhi18av/book-keeper" 
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.9.0"]])
---
+++ src/book_keeper/core.clj
@@ -0,0 +1,6 @@
+(ns book-keeper.core)
+
+(defn foo
+  "I don't do a whole lot."
+  [x]
+  (println x "Hello, World!"))
