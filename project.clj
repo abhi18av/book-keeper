d9cffde78f56073f4d28dccd3a0c509dad58d18c
Abhinav Sharma <abhi18av@gmail.com>
2018-06-23 21:49:42 +0530

    trying out my hands with the datomic samples now

--- project.clj
+++ project.clj
@@ -1,6 +1,25 @@
 (defproject book-keeper "0.1.0-SNAPSHOT"
+
   :description "A clojure repl for managing my personal e-library"
-  :url "https://github.com/abhi18av/book-keeper" 
+
+  :url "https://github.com/abhi18av/book-keeper"
+
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.9.0"]])
+
+
+;; Install the peer library by running ./bin/maven-install from the datomic-free/pro root folder
+  :repositories {"my.datomic.com" {:url "https://my.datomic.com/repo"
+                                 :creds :gpg}}
+  :dependencies [
+  [org.clojure/clojure "1.9.0"]
+
+
+  [com.datomic/datomic-free "0.9.5703"]
+
+
+  [com.datomic/datomic-pro "0.9.5703"]
+
+  ;; Rules engine
+  ;[com.cerner/clara-rules "0.18.0"]
+  ])
