dcdefe9207e2463ef59ef6533dcc6e39c2ba9f3c
Abhinav Sharma <abhi18av@gmail.com>
2018-07-02 12:49:12 +0530

    removed the resources files in favor of progressLog

--- _scratch/dl4j.clj
+++ _scratch/dl4j.clj
@@ -4,21 +4,21 @@
 ;;Example configuration of a simple multilayer feedforward neural network architecture
 ;;classifying the iris dataset.
 (def iris-net-config
-	[:layers 
-	 [[:dense [:n-in 4 :n-out 10 :activation :relu]]
-	 	[:dense [:n-in 10 :n-out 10 :activation :relu]]
-	 	[:output :negative-log-likelihood [:n-in 10 :n-out 3 :activation :softmax]]]])
+  [:layers
+   [[:dense [:n-in 4 :n-out 10 :activation :relu]]
+    [:dense [:n-in 10 :n-out 10 :activation :relu]]
+    [:output :negative-log-likelihood [:n-in 10 :n-out 3 :activation :softmax]]]])
 
 (def iris-net
-	(-> iris-net-config
-		  ai/network
-		  ai/initialize-net))
+  (-> iris-net-config
+      ai/network
+      ai/initialize-net))
 
 (defn iris-train []
   (let [iris-iterator (ai/classification-csv-iterator "iris.csv" 150 4 3)]
     (-> iris-net
-	      (ai/train-net! 200 iris-iterator)
-	      (ai/save-model "iris-model"))
+        (ai/train-net! 200 iris-iterator)
+        (ai/save-model "iris-model"))
     (.reset iris-iterator)
     (println (ai/evaluate iris-net iris-iterator))))
 
@@ -26,6 +26,3 @@
 
 
 ;;;;;;;
-
-
-
